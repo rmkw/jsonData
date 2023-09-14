77a5476e92a29efe2c12a3c63038943c065b1303
rmkw <creditozona1@gmail.com>
2023-09-14 09:48:00 -0600

    :rocket::alien: implement fuze

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const jsonServer = require("json-server"); // importing json-server library
+const Fuse = require("fuse.js");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use(jsonServer.bodyParser); // Agrega el middleware bodyParser para manejar datos POST
+
+// Define una nueva ruta para la búsqueda difusa
+server.get("/fuzzy-search", (req, res) => {
+    const query = req.query.q; // Obtiene el término de búsqueda desde la URL
+    if (!query) {
+        res.status(400).json({ error: "El parámetro 'q' es requerido" });
+        return;
+    }
+
+    const data = router.db.getState().yourData; // Reemplaza 'yourData' con el nombre de tu colección de datos
+    const options = {
+        // Configura las opciones de búsqueda difusa con Fuse.js
+        shouldSort: true,
+        threshold: 0.6,
+        keys: ["nombre"], // Reemplaza 'nombre' con el nombre del campo en tus datos que deseas buscar
+    };
+
+    const fuse = new Fuse(data, options);
+    const result = fuse.search(query);
+
+    res.json(result);
+});
 server.use(router);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+});
